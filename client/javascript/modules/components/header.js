c41094f388c3ba3de61eed164f1527bd4bdf917d
Eirik Vullum <evullum@gmail.com>
2014-08-31 22:16:42 +0200

    add link to app from header

--- client/javascript/modules/components/header.js
+++ client/javascript/modules/components/header.js
@@ -28,7 +28,7 @@ module.exports = React.createClass({
                             <span className='icon-bar'></span>
                             <span className='icon-bar'></span>
                         </button>
-                        <a className='navbar-brand' href='#'>Ambition</a>
+                        <a className='navbar-brand' href='/home'>Ambition</a>
                     </div>
 
                     <div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
